1aacf526b0d38a512c53049df0a05deffcaaf4b6
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-09 18:26:01 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -20,18 +20,33 @@ def img(query):
     }
     return(urel)
     
-def searchGoogle(query);
-		URL = f"https://google.com/search?q={query}"
-		headers = {"user-agent": hander}
-		resp = requests.get(URL, headers=headers)
-		if resp.status_code == 200:
-			soup = BeautifulSoup(resp.content, "html.parser")
-			anu =[]
-			for g in soup.find_all('div', class_='r'):
-				anchors = g.find_all('a')
-				anu.append(anchors[0]['href'])
-			result ={
-				'link': '%s'%(anu),
-				'title': '%s'%(g.find('h3').text)
-			}
-			return(result)
+def instaprofile(user):
+    uReq = requests
+    bSoup = BeautifulSoup
+    website = uReq.get("https://www.instagram.com/{}/".format(str(user)))
+    data = bSoup(website.content, "lxml")
+    for getInfoInstagram in data.findAll("script", {"type":"text/javascript"})[3]:
+        getJsonInstagram = re.search(r'window._sharedData\s*=\s*(\{.+\})\s*;', getInfoInstagram).group(1)
+        data = json.loads(getJsonInstagram)
+        for instagramProfile in data["entry_data"]["ProfilePage"]:
+    	    username = instagramProfile["graphql"]["user"]["username"]
+    	    name = instagramProfile["graphql"]["user"]["full_name"]
+    	    picture = instagramProfile["graphql"]["user"]["profile_pic_url_hd"]
+    	    biography = instagramProfile["graphql"]["user"]["biography"]
+    	    followers = instagramProfile["graphql"]["user"]["edge_followed_by"]["count"]
+    	    following = instagramProfile["graphql"]["user"]["edge_follow"]["count"]
+    	    private = instagramProfile["graphql"]["user"]["is_private"]
+    	    media = instagramProfile["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
+    	    result = {
+    			"result": {
+    				"username": username,
+    				"fullname": name,
+    				"bio": biography,
+    				"followers": followers,
+    				"following": following,
+    				"media": media,
+    				"private": private,
+    				"profile_img": picture
+    			}
+    		}
+    	    return(result)
