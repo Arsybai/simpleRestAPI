e40ebde21bed6c7dd9e2f52397b2885cc5ff1f80
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-09 19:50:02 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -7,30 +7,19 @@ from urllib.parse import quote, unquote, re
 hander = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
 def img(query):
-    Image =[]
-    r = requests.get("https://api.qwant.com/api/search/images",
-    	params={
-        	'count': 50,
-        	'q': query,
-        	't': 'images',
-        	'safesearch': 1,
-        	'locale': 'en_US',
-        	'uiv': 4
-    	},
-    	headers={
-    		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-    	}
-    )
-    response = r.json().get('data').get('result').get('items')
-    urls = [r.get('media') for r in response]
-    for cok in urls:
-    	Image.append(cok)
-    ret = {
-    	'resulte': {'link': '%s'%(Image)},
-    	'creator': 'geo Usa, rey and Fino',
-        'status: 'OKE'
-    }
-    return(ret)
+	imagedata = []
+	link = 'https://www.google.co.in/search?q={}&source=lnms&tbm=isch'.format(query)
+	soup = requests.get(link)
+	soup = BeautifulSoup(soup.content,"lxml")
+	for clu in soup.findAll('img'):
+		if 'textinput' not in clu['src']:
+			imagedata.append(clu['src'])
+	result = {
+		'status':'OK',
+        'creato': 'geo, rey, fino'
+		'result':imagedata
+		}
+	return(result)
     
 def instaprofile(user):
     uReq = requests
