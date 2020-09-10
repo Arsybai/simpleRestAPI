2b501f5dd443c0e6d0c583c75bd71ac0092c5ef6
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-10 20:07:17 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -50,7 +50,6 @@ def rest_sendValday():
 def rest_sendNews():
 	this_path = request.args['path']
 	this_text = request.args['text']
-	this_text1 = request.args['text1']
 	this_text2 = request.args['text2']
 	this_text3 = request.args['text3']
 	this_rest = scrap.sendNews(this_path, this_text, this_text2, this_text3)
