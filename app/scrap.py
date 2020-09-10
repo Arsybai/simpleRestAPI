55301713f1f4efe1522631c25574c1dd8402220b
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-10 19:09:02 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -119,3 +119,26 @@ def textVideo(query):
 	except:
 		result = {"result": "Error info id Iine denmas_geo"}
 		return(result)
+	
+	def sendValday(path,text):
+	try:
+		headers = {"user-agent": hander}
+		link = "https://m.photofunia.com/categories/valentines_day/valentine"
+		option = {'effect-form js-effect-form': 'input-file', 'image': path,'text': text, 'button-container': 'GO'}
+		ck = requests.post(link,option, headers=headers).text
+		cok = line.getStr(ck,'<div class="image full-height-container">','</div>')
+		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_~@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', cok)
+		for url in urls:
+			mek = unquote(url).replace("%", "")
+			res = requests.get('https://tinyurl.com/api-create.php?url=%s' % mek)
+			result = {
+				"result": {
+					"linkUrl": res.text
+				},
+				"creator": "geo, rey",
+				"status": "OKE COK___!"
+			}
+		return(result)
+	except:
+		result = {"result": "Error info id Iine denmas_geo"}
+		return(result)
