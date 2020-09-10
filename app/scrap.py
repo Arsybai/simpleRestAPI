06c23473816a20f23d40ab8c946c79171a408549
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-11 00:33:53 +0700

    Add files via upload

--- app/scrap.py
+++ app/scrap.py
@@ -1,166 +1,48 @@
-import requests, re, json, random
+import requests, re, json
 from bs4 import BeautifulSoup
 from re import match
-from urllib.parse import urlparse
-from urllib.parse import quote, unquote, re
 
-hander = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
-headers = {
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-	'Accept-Language': 'id,en-US;q=0.7,en;q=0.3',
-	'Connection': 'keep-alive',
-	'Upgrade-Insecure-Requests': '1',
-	'Pragma': 'no-cache',
-	'Cache-Control': 'no-cache',
-	'TE': 'Trailers',
-}
-
-def getStr(string,start,end, index = 1):
-	try:
-		str = string.split(start)
-		str = str[index].split(end)
-		return str[0]
-	except:pass
-	
 def img(query):
 	imagedata = []
-	link = requests.get("https://api.qwant.com/api/search/images",
-	    params={
-        	'count': 50,
-        	'q': query,
-        	't': 'images',
-        	'safesearch': 1,
-        	'locale': 'en_US',
-        	'uiv': 4
-	    },
-	    headers = {
-    		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-	    }
-	)
-	response = link.json().get('data').get('result').get('items')
-	urls = [link.get('media') for link in response]
-	for cok in urls:
-	   ret = {
-	   	"result": "%s"%(cok),
-	   	"Status": "OKE COK___!",
-	   	"creator":"geo, rey and Fino"
-	   }
-	   imagedata.append(ret)
-	return(imagedata)
-
-def randomimg(query):
-	link = f"https://source.unsplash.com/random/900×700/?{query}"
-	res = requests.get('https://tinyurl.com/api-create.php?url=%s' % link)
+	link = 'https://www.google.co.in/search?q={}&source=lnms&tbm=isch'.format(query)
+	soup = requests.get(link)
+	soup = BeautifulSoup(soup.content,"lxml")
+	for clu in soup.findAll('img'):
+		if 'textinput' not in clu['src']:
+			imagedata.append(clu['src'])
 	result = {
-		"linkUrl": res.text,
-		"creator": "geo",
-		"status": "OKE COK___!"
-	}
-	return(result)
-
-def artiName(nama):
-	try:
-		dsa ="http://primbon.com/arti_nama.php?nama1={}&proses=+Submit%21+".format(nama)
-		response = requests.get(dsa, headers=headers).text
-		anu = getStr(response, '<b>ARTI NAMA</b><br><br>', '<TABLE>')
-		text=str(anu).replace("<b><i>",": ").replace("</i></b>","").replace("<br><br>","").replace(".<br><br>","").replace("memiliki arti:","")
-		result = {
-			"result":{
-				"arti": text,
-				"name": nama
-			},
-			"creator": "geo",
-			"status": "OKE COK___!"
+		'status':'OK',
+		'result':imagedata
 		}
-		return(result)
-	except:
-		result = {"result": "Error info id Iine denmas_geo"}
-		return(result)
-		
-def goSearch(query):
-    headers = {"user-agent" : hander}
-    resp = requests.get(f"https://google.com/search?q={query}", headers=headers)
-    if resp.status_code == 200:
-    	soup = BeautifulSoup(resp.content, "html.parser")
-    	results = []
-    	for g in soup.find_all('div', class_='r'):
-    		anchors = g.find_all('a')
-    		if anchors:
-    		  link = anchors[0]['href']
-    		  title = g.find('h3').text
-    		  item = {
-    		  	"title": title,
-    		  	"link": link
-    		  }
-    		  results.append(item)
-    	return(results)
-def textVideo(query):
-	try:
-		headers = {"user-agent": hander}
-		link = "https://photooxy.com/league-of-legends/create-avatar-video-with-animation-lol-champions-180.html"
-		numku = str(random.randint(1, 23))
-		option = {'optionNumber_0': numku, 'optionNumber_1': '1', 'text_2': query, 'login': 'OK'}
-		geo = requests.post(link,option, headers=headers).text
-		recu = getStr(geo,'<div class="alert alert-info" role="alert">','<a class = "btn btn-default"')
-		requ = recu.split("-")[0]
-		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_~@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', requ)
-		for url in urls:
-			mek = unquote(url).replace("%", "")
-			res = requests.get('https://tinyurl.com/api-create.php?url=%s' % mek)
-			result = {
-				"result": {
-					"linkUrl": res.text
-				},
-				"creator": "geo, rey",
-				"status": "OKE COK___!"
-			}
-		return(result)
-	except:
-		result = {"result": "Error info id Iine denmas_geo"}
-		return(result)
-def sendValday(path,text):
-	try:
-		headers = {"user-agent": hander}
-		link = "https://m.photofunia.com/categories/valentines_day/valentine"
-		option = {'effect-form js-effect-form': 'input-file', 'image': path,'text': text, 'button-container': 'GO'}
-		ck = requests.post(link,option, headers=headers).text
-		cok = getStr(ck,'<div class="image full-height-container">','</div>')
-		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_~@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', cok)
-		for url in urls:
-			mek = unquote(url).replace("%", "")
-			res = requests.get('https://tinyurl.com/api-create.php?url=%s' % mek)
-			result = {
-				"result": {
-					"linkUrl": res.text
-				},
-				"creator": "geo, rey, hans, fino",
-				"status": "OKE COK___!"
-			}
-		return(result)
-	except:
-		result = {"result": "Error info id Iine denmas_geo"}
-		return(result)
-def sendNews(path,text,text2,text3):
-	try:
-		headers = {"user-agent": hander}
-		link = "https://m.photofunia.com/categories/all_effects/breaking-news"
-		option = {'effect-form js-effect-form': 'input-file', 'image': path,'field text-field': 'text', 'channel': text,'title1': text2,'title2': text3, 'button-container': 'GO'}
-		usa = requests.post(link,option, headers=headers).text
-		cok = getStr(usa,'<div class="image full-height-container">','</div>')
-		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_~@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', cok)
-		for url in urls:
-			mek = unquote(url).replace("%", "")
-			res = requests.get('https://tinyurl.com/api-create.php?url=%s' % mek)
-			result = {
-				"result": {
-					"linkUrl": res.text
-				},
-				"creator": "geo, rey, hans, fino",
-				"status": "OKE COK___!"
-			}
-		return(result)
-	except:
-		result = {"result": "Error info id Iine denmas_geo"}
-		return(result)
+	return(result)
 	
+def instaprofile(user):
+    uReq = requests
+    bSoup = BeautifulSoup
+    website = uReq.get("https://www.instagram.com/{}/".format(str(user)))
+    data = bSoup(website.content, "lxml")
+    for getInfoInstagram in data.findAll("script", {"type":"text/javascript"})[3]:
+        getJsonInstagram = re.search(r'window._sharedData\s*=\s*(\{.+\})\s*;', getInfoInstagram).group(1)
+        data = json.loads(getJsonInstagram)
+        for instagramProfile in data["entry_data"]["ProfilePage"]:
+    	    username = instagramProfile["graphql"]["user"]["username"]
+    	    name = instagramProfile["graphql"]["user"]["full_name"]
+    	    picture = instagramProfile["graphql"]["user"]["profile_pic_url_hd"]
+    	    biography = instagramProfile["graphql"]["user"]["biography"]
+    	    followers = instagramProfile["graphql"]["user"]["edge_followed_by"]["count"]
+    	    following = instagramProfile["graphql"]["user"]["edge_follow"]["count"]
+    	    private = instagramProfile["graphql"]["user"]["is_private"]
+    	    media = instagramProfile["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
+    	    result = {
+    			"result": {
+    				"username": username,
+    				"fullname": name,
+    				"bio": biography,
+    				"followers": followers,
+    				"following": following,
+    				"media": media,
+    				"private": private,
+    				"profile_img": picture
+    			}
+    		}
+    	    return(result)
