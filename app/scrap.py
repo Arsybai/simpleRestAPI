6bf0c0ed4e0f531846138ce034c92e4ea561ad62
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-11 22:08:31 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -1,4 +1,4 @@
-import requests, re, json, random, pafy, urllib
+import requests, re, json, random, pafy
 from bs4 import BeautifulSoup
 from re import match
 from urllib.parse import urlparse
@@ -95,32 +95,6 @@ def goSearch(query):
     		  }
     		  results.append(item)
     	return(results)
-		
-def stafa(search):
-	try:
-		results = []
-		URL = f"https://m.stafabandt.site/mp3/{search}.html"
-		headers = {"user-agent": header}
-		resp = requests.get(URL, headers=headers)
-		soup = BeautifulSoup(resp.content, "html5lib")
-		data = soup.select("a")
-		for b in data:
-			if "https://m.stafabandt.site/link/" in str(b):
-				title = b.get("title")
-				link = b.get("href")
-				item = {
-    		  	"title": title,
-    		  	"link": link
-				}
-				results.append(item)
-		data ={
-			"result":{
-				"main": results
-			},
-			"creator": "geo, hans, fino",
-			"status": "OKE COK___!"
-		}
-		return(data)
 					
 def textVideo(query):
 	try:
