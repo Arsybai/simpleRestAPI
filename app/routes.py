b1e39ae1e88b7c55be1d56050179452c73787843
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-11 21:56:58 +0700

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -15,9 +15,48 @@ def rest_image():
 	this_rest = scrap.img(this_query)
 	return json.dumps(this_rest, indent=4)
 	
-@app.route('/instagram',methods=['POST','GET'])
-def rest_insta():
-	this_user = request.args['username']
-	this_rest = scrap.instaprofile(this_user)
+@app.route('/stafa',methods=['POST','GET'])
+def rest_stafa():
+	this_query = request.args['search']
+	this_rest = scrap.stafa(this_query)
 	return json.dumps(this_rest, indent=4)
 	
+@app.route('/randomimg',methods=['POST','GET'])
+def rest_randomimg():
+	this_query = request.args['query']
+	this_rest = scrap.randomimg(this_query)
+	return json.dumps(this_rest, indent=4)
+
+@app.route('/artiname',methods=['POST','GET'])
+def rest_artiName():
+	this_query = request.args['query']
+	this_rest = scrap.artiName(this_query)
+	return json.dumps(this_rest, indent=4)
+
+@app.route('/googlesearch',methods=['POST','GET'])
+def rest_goSearch():
+	this_query = request.args['query']
+	this_rest = scrap.goSearch(this_query)
+	return json.dumps(this_rest, indent=4)
+	
+@app.route('/textavatar',methods=['POST','GET'])
+def rest_textVideo():
+	this_query = request.args['query']
+	this_rest = scrap.textVideo(this_query)
+	return json.dumps(this_rest, indent=4)
+
+@app.route('/photofun/valentines_day',methods=['POST','GET'])
+def rest_sendValday():
+	this_path = request.args['path']
+	this_query = request.args['text']
+	this_rest = scrap.sendValday(this_path, this_query)
+	return json.dumps(this_rest, indent=4)
+	
+@app.route('/photofun/breaking-news',methods=['POST','GET'])
+def rest_sendNews():
+	this_path = request.args['path']
+	this_text = request.args['text']
+	this_text2 = request.args['text2']
+	this_text3 = request.args['text3']
+	this_rest = scrap.sendNews(this_path, this_text, this_text2, this_text3)
+	return json.dumps(this_rest, indent=4)
