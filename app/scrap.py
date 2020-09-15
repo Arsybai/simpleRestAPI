12de6b99d87afcf11bc81f17a15ace91c4cd3558
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-16 02:19:55 +0700

    Add files via upload

--- app/scrap.py
+++ app/scrap.py
@@ -827,3 +827,62 @@ def senddaily(path,text,text1):
 	except:
 		result = {"result": "Error info id Iine denmas_geo"}
 		return(result)
+#====[qr login]=====
+class NewQRLogin:
+    API_URL = "https://api.lrtt.icu/secondaryQrCodeLogin.do"
+    HEADERS = {
+        "android_lite": {
+            "User-Agent": "LLA/2.12.0 SKR-H0 9",
+            "X-Line-Application": "ANDROIDLITE\t2.12.0\tAndroid OS\t9;SECONDARY"
+        },
+        "android": {
+            "User-Agent": "Line/10.6.2",
+            "X-Line-Application": "ANDROID\t10.6.2\tAndroid OS\t10"
+        },
+        "ios_ipad": {
+            "User-Agent": "Line/10.1.1",
+            "X-Line-Application": "IOSIPAD\t10.1.1\tiPhone 8\t11.2.5"
+        },
+        "ios": {
+            "User-Agent": "Line/10.1.1",
+            "X-Line-Application": "IOS\t10.1.1\tiPhone 8\t11.2.5"
+        },
+        "chrome": {
+            "User-Agent": "MozilX-Line-Application/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36",
+            "X-Line-Application": "CHROMEOS\t2.3.2\tChrome OS\t1"
+        },
+        "desktopwin": {
+            "User-Agent": "Line/5.12.3",
+            "X-Line-Application": "DESKTOPWIN\t5.21.3\tWindows\t10"
+        },
+        "desktopmac": {
+            "User-Agent": "Line/5.12.3",
+            "X-Line-Application": "DESKTOPMAC\t5.21.3\tMAC\t10.15"
+        }
+    }
+
+    def parseLogin(this, loginInfo):
+        return (loginInfo["token"], loginInfo["certificate"])
+        
+    def loginWithQrCode(self, header, certificate="", callback=lambda output: print(output)):
+        assert header in self.HEADERS, "invaild header"
+        resp = requests.post(self.API_URL + "/login?" + urllib.parse.urlencode({"custom_headers": self.HEADERS[header], "certificate": certificate}))
+        res = resp.json()
+        if resp.status_code != 200:
+            raise Exception(res)
+        result = {
+            "result": "%s" % (res["url"])
+        }
+        return(result)
+        while "token" not in res:
+            resp = requests.post(self.API_URL + res["callback"])
+            res = resp.json()
+            if resp.status_code != 200:
+                raise Exception(res)
+            if "pin" in res:
+                result = {
+                    "pincode": "%s"%(res["pin"])
+                }
+                return(result)
+        return self.parseLogin(res)
+        
