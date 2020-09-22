406b116ba1d1262499436d671b97a8b2f0991c28
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-22 23:41:42 +0700

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -282,16 +282,19 @@ def rest_sendPhotoxy_349():
 	this_path = request.args['url']
 	this_rest = scrap.sendPhotoxy_349(this_path)
 	return json.dumps(this_rest, indent=4, sort_keys=True)
+
 @app.route('/api/mockup',methods=['POST','GET'])
 def rest_sendPhotoxy_398():
 	this_path = request.args['url']
 	this_rest = scrap.sendPhotoxy_398(this_path)
 	return json.dumps(this_rest, indent=4, sort_keys=True)
+	
+
 @app.route('/api/pokemon',methods=['POST','GET'])
 def rest_sendPhotoxy_148():
 	this_path = request.args['url']
 	this_rest = scrap.sendPhotoxy_148(this_path)
-	return json.dumps(this_rest, indent=4, sort_keys=True)
+	return json.dumps(this_rest, indent=4)
 	
 #===========[FOTOFUNIA]============
 @app.route('/photofun/breaking-news',methods=['POST','GET'])
