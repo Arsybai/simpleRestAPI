f6d9300f72f7b6ff21dee1ecf8bae2ba4c7f4ed9
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2020-09-24 22:14:29 +0700

    Add files via upload

--- app/scrap.py
+++ app/scrap.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     import urllib2
     
-_session = requests.session()
+#_session = requests.session()
 hander = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 }
